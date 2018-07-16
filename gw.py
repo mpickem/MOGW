2a833975dc8b99e44b3532f795ac43f4b2ea0535
Matthias Pickem <matthias.pickem@gmail.com>
2018-07-16 18:37:02 +0200

    Added mpi4py stuff.

--- gw.py
+++ gw.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function, division, absolute_import
 import numpy as np
+from mpi4py import MPI
 import h5py
 import sys
 import time
@@ -9,3 +10,32 @@ import time
 from src.glb import mglobal
 from src.ham import mhamil
 from src.comp import mcomp
+
+'''
+Main wrapper for the underlying fortran files
+found in src. Calculation of the Selfenergy approximation GW
+in the Wannier Basis
+'''
+
+# mpi communicator, rank, size + data distribution
+comm = MPI.COMM_WORLD
+mpi_rank = comm.Get_rank()
+mpi_size = comm.Get_size()
+
+data_interval = 31 # we get this number from the hamiltonian and/or input
+
+displ=[] # displacement
+displ.append(0)
+rct=[]   # receive count
+
+# distribution of data_interval
+for i in xrange(mpi_size-1):
+  rct.append((data_interval-displ[i])//(mpi_size-i))
+  displ.append(rct[i]+displ[i])
+rct.append(data_interval - displ[mpi_size-1])
+
+rct = np.array(rct)
+displ = np.array(displ)
+
+mglobal.ikstart = displ[mpi_rank]
+mglobal.ikend   = displ[mpi_rank] + rct[mpi_rank]
